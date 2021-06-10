65c8c3e9dc03ad3187547043e12909cf476825d3
sravan reddy <sravanreddy234@gmail.com>
2021-06-10 13:17:39 +0530

    added some advanced features
    Included Exceptions Handling, Validation, HATEOAS, Internationalization (i18n), Content Negotiation, Auto generation of SWAGGER Documentation, Spring Boot Actuator

--- src/main/java/com/practice/restful/webservice/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/practice/restful/webservice/restfulwebservices/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.practice.restful.webservice.restfulwebservices;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {		
@@ -20,5 +29,10 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world-bean/{name}")
 	public HelloWorldBean helloWorldBean(@PathVariable String name) {	
 		return new HelloWorldBean("Hello World "+name);
+	}
+	
+	@GetMapping(path="/hello-world-Internationalized")
+	public String helloWorldInternationalized(/*@RequestHeader(name="Accept-Language",required=false) Locale locale*/) {		
+		return messageSource.getMessage("good.morning.message", null, "Default Message", /*locale*/ LocaleContextHolder.getLocale());
 	}
 }
---
+++ src/main/java/com/practice/userProject/User.java
@@ -0,0 +1,47 @@
+package com.practice.userProject;
+
+import java.util.Date;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Past;
+
+public class User {
+	
+	private Integer id;
+	
+	@Min(value =2, message = "Name should have atleast 2 characters")
+	private String name;
+	
+	@Past
+	private Date birthDate;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public User(Integer id, String name, Date birthDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+	
+	protected User() {
+		
+	}
+
+}
